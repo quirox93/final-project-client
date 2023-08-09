0261987da8ffd80403ff065aa752966403048e25
Luciano E. Perez <info.luciando.design@gmail.com>
2023-08-09 20:36:45 -0300

    feature: products , product , tailwindstyles

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,12 +9,27 @@ export default function Product(props) {
       <div className="ml-5 flex-1 space-y-3">
         <h2 className="text-lg font-bold">{props.name}</h2>
         <p>{props.description}</p>
-        <p>Price: <span className="text-green font-bold">${props.price}</span></p>
-        {props.stock === 0 ? <span className="bg-black rounded-2xl text-white p-1">Out of Stock</span> : <span className="bg-green rounded-2xl text-white p-1">Available</span>}
-        <p>Stock: {
-          props.stock === 0 ? <span className="text-red">0</span>  : <span className="text-green">{props.stock}</span>
-        }</p>
-        <button className="w-12 bg-twitter-blue rounded text-white py-2 px-4">+</button>
+        <p>
+          Price: <span className="text-green font-bold">${props.price}</span>
+        </p>
+        {props.stock === 0 ? (
+          <span className="bg-black rounded-2xl text-white p-1">
+            Out of Stock
+          </span>
+        ) : (
+          <span className="bg-green rounded-2xl text-white p-1">Available</span>
+        )}
+        <p>
+          Stock:{" "}
+          {props.stock === 0 ? (
+            <span className="text-red">0</span>
+          ) : (
+            <span className="text-green">{props.stock}</span>
+          )}
+        </p>
+        <button className="w-12 bg-twitter-blue rounded text-white py-2 px-4">
+          +
+        </button>
       </div>
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,36 +1,33 @@
-'use client'
+"use client";
 
 import React, { useState, useEffect } from "react";
 import Product from "./Product";
 import axios from "axios";
 
-
-
-
 export default function Products() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://restapicrud.ericksegura5.repl.co/products/")
-          .then(response => {
-            setProducts(response.data);
-          })
-          .catch(error => {
-            console.error("Error fetching products:", error);
-          });
-      }, []);
-  
+  useEffect(() => {
+    axios
+      .get("https://restapicrud.ericksegura5.repl.co/products/")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
   return (
     <div className="flex flex-wrap ">
-      
-      {products.map(product => (
+      {products.map((product) => (
         <Product
-          key={product._id} 
+          key={product._id}
           name={product.name}
           description={product.description}
           price={product.price}
           stock={product.stock}
-          image={product.imag.secure_url} 
+          image={product.imag.secure_url}
         />
       ))}
     </div>
