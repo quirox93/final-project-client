e7b328340b22a80a3b599213f7cf1db90df68ed0
Sir0s <maty.solis87@gmail.com>
2023-08-09 20:43:20 -0300

    navBar es un compoenente ahora

--- src/app/Form/page.jsx
+++ src/app/Form/page.jsx
@@ -16,7 +16,7 @@ function Form() {
     formData.append('description', description);
     formData.append('price', price);
     formData.append('imag', image);
-    console.log(formData)
+    
     try {
       const response = await axios.post('https://restapicrud.ericksegura5.repl.co/products/', formData, {
         headers: {
