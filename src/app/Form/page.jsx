23b70d432876872e2941481dc8925793fbb91516
Sir0s <maty.solis87@gmail.com>
2023-08-09 19:33:50 -0300

    navs y form

---
+++ src/app/Form/page.jsx
@@ -0,0 +1,79 @@
+"use client"
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Form() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('price', price);
+    formData.append('imag', image);
+    console.log(formData)
+    try {
+      const response = await axios.post('https://restapicrud.ericksegura5.repl.co/products/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data); // New product data from the server
+
+      // Clear form after successful submission
+      setName('');
+      setDescription('');
+      setPrice(0);
+      setStock(0);
+      setImage([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Price:</label>
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </div>
+  
+      <div>
+        <label>Image:</label>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
