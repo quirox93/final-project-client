27193df8ac3e2840c71875379cd0a2abe7416f5d
warben <quirox01@gmail.com>
2023-08-09 21:27:43 -0300

    feat: Cloudinary uploader

---
+++ src/app/cloud/page.jsx
@@ -0,0 +1,61 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Cloud() {
+  const [imageSrc, setImageSrc] = useState();
+  const [uploadData, setUploadData] = useState("");
+
+  function handleOnChange(event) {
+    const reader = new FileReader();
+
+    reader.onload = function (onLoadEvent) {
+      setImageSrc(onLoadEvent.target.result);
+      setUploadData("");
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  async function handleOnSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const fileInput = Array.from(form.elements).find(({ name }) => name === "file");
+
+    const formData = new FormData();
+
+    for (const file of fileInput.files) {
+      formData.append("file", file);
+    }
+
+    formData.append("upload_preset", "wo8ssx61");
+
+    const data = await fetch("https://api.cloudinary.com/v1_1/dpws83hey/image/upload", {
+      method: "POST",
+      body: formData,
+    }).then((r) => r.json());
+
+    setUploadData({ public_id: data.public_id, secure_url: data.secure_url });
+  }
+
+  return (
+    <div>
+      <form method="post" onSubmit={handleOnSubmit}>
+        <p>
+          <input onChange={handleOnChange} type="file" name="file" />
+        </p>
+        {imageSrc && <Image width={150} height={150} alt={uploadData.public_id} src={imageSrc} />}
+
+        <p>
+          <button>Add Product</button>
+        </p>
+      </form>
+      {uploadData && (
+        <code>
+          <pre>{JSON.stringify(uploadData, null, 2)}</pre>
+        </code>
+      )}
+    </div>
+  );
+}
