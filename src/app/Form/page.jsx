abf037715414dd2b33e856fe474a8ae53578d8a5
Sir0s <maty.solis87@gmail.com>
2023-08-09 22:01:27 -0300

    agregados css styles

--- src/app/Form/page.jsx
+++ src/app/Form/page.jsx
@@ -37,40 +37,54 @@ function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name:</label>
+    <form className="max-w-md mx-auto p-4 border rounded-md shadow-md" onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label className="block font-bold mb-1">Name:</label>
         <input
           type="text"
+          className="w-full p-2 border rounded-md"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </div>
-      <div>
-        <label>Description:</label>
+      <div className="mb-4">
+        <label className="block font-bold mb-1">Description:</label>
         <textarea
+          className="w-full p-2 border rounded-md"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
       </div>
-      <div>
-        <label>Price:</label>
+      <div className="mb-4">
+        <label className="block font-bold mb-1">Price:</label>
         <input
           type="number"
+          className="w-20 p-2 border rounded-md"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
       </div>
   
-      <div>
-        <label>Image:</label>
+      <div className="mb-4">
+        <label className="block font-bold mb-1">Image:</label>
         <input
           type="file"
           accept="image/*"
+          className="w-full p-2 border rounded-md"
           onChange={(e) => setImage(e.target.files[0])}
         />
       </div>
-      <button type="submit">Submit</button>
+      
+      <div className="flex justify-center">
+        <div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-black font-semibold py-2 px-6 rounded-full border-2 border-blue-500 hover:border-blue-600 transition duration-300 ease-in-out"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
