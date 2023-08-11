a0a9cdbcdd446ba7d28df5e2e97dedb9f19abd58
warben <quirox01@gmail.com>
2023-08-11 20:08:52 -0300

    feat: paginado + reactIcons

--- src/components/DashProducts.jsx
+++ src/components/DashProducts.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Pagination, PaginationItem, PaginationCursor } from "@nextui-org/react";
 import React, { useState, useEffect } from "react";
 import DashProduct from "./DashProduct";
 import DetailDashProduct from "./DetailDashProduct";
@@ -7,22 +7,42 @@ import axios from "axios";
 import DashText from "./DashProduct/DashText";
 
 export default function DashProducts() {
+  const items = 2;
   const [dashProducts, setDashProducts] = useState([]);
-
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const map = dashProducts.map((product) => (
+    <li key={product._id} className="gap-1 grid grid-flow-col grid-cols-5">
+      <DashProduct
+        id={product._id}
+        name={product.name}
+        description={product.description}
+        price={product.price}
+        stock={product.stock}
+        image={product.imag.secure_url}
+        date={product.createdAt}
+      />
+    </li>
+  ));
   useEffect(() => {
+    //setDashProducts([]);
     axios
-      .get("api/product")
+      .get(`https://restapicrud.ericksegura5.repl.co/products?page=${page}&count=${items}`)
       .then((response) => {
-        setDashProducts(response.data);
+        setDashProducts(response.data.results);
+        setTotal(Math.ceil(response.data.total / items));
       })
       .catch((error) => {
         console.error("Error fetching products:", error);
       });
-  }, []);
+  }, [page]);
 
   return (
     <div>
       <ul role="list" className="divide-y divide-default">
+        <div className="flex justify-center">
+          <Pagination onChange={setPage} total={total} page={page} initialPage={1} />
+          </div>
         <li className="gap-1 grid grid-flow-col grid-cols-5">
           <DashText info="Name" />
           <DashText info="Price" />
@@ -30,19 +50,8 @@ export default function DashProducts() {
           <DashText info="Date" />
           <DashText info="Actions" />
         </li>
-        {dashProducts.map((product) => (
-          <li key={product._id} className="gap-1 grid grid-flow-col grid-cols-5">
-            <DashProduct
-              id={product._id}
-              name={product.name}
-              description={product.description}
-              price={product.price}
-              stock={product.stock}
-              image={product.imag.secure_url}
-              date={product.createdAt}
-            />
-          </li>
-        ))}
+        {map}
+        <li></li>
       </ul>
       <DetailDashProduct />
     </div>
