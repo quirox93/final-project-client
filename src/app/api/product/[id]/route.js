6d1d980ec7976ea9e12df1335e07f468a1c8fb04
warben <quirox01@gmail.com>
2023-08-13 17:14:04 -0300

    fix api

--- src/app/api/product/[id]/route.js
+++ src/app/api/product/[id]/route.js
@@ -19,40 +19,6 @@ export async function PUT(req, { params }) {
     connectDB();
     const data = await req.formData();
     const values = Object.fromEntries(data);
-    delete values.imag;
-    const file = data.get("imag");
-    if (file) {
-      const { public_id, secure_url } = await uploadImag(file);
-      values.imag = { public_id, secure_url };
-    }
-    const product = await Product.findByIdAndUpdate(params.id, values);
-    return NextResponse.json(product);
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
-
-const { NextResponse } = require("next/server");
-import { connectDB } from "@/utils/mongoose";
-import Product from "@/models/Product";
-import { deleteImag, uploadImag } from "@/utils/cloudinary";
-
-export async function GET(_, { params }) {
-  try {
-    connectDB();
-    const products = await Product.findById(params.id);
-    if (!products) return NextResponse.json({ message: "Product not found " }, { status: 404 });
-    return NextResponse.json(products);
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
-
-export async function PUT(req, { params }) {
-  try {
-    connectDB();
-    const data = await req.formData();
-    const values = Object.fromEntries(data);
     const file = data.get("imag");
     if (file) {
       const { public_id, secure_url } = await uploadImag(file);
