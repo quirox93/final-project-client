f3952c28005c9f78440026535a2f8f8df98cb63a
camiloduquee <camiloduquee@gmail.com>
2023-08-13 11:10:06 -0400

    update: dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,19 +1,32 @@
 import DashProducts from "@/components/DashProducts";
 import Sidebar from "@/components/Sidebar";
 import HeaderDash from "@/components/HeaderDash";
-import Filters from "@/components/Filters";
+import SearchBar from "@/components/SearchBar";
+
+import FilterModal from "@/components/FilterModal";
+// import SortNameButton from "@/components/SortNameButton";
+// import SortPriceButton from "@/components/SortPriceButton";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
 export default function Dashboard() {
   return (
-    <div className="flex  max-w-full h-screen ">
-      <div>
+    <div className="flex">
+      <div className="flex-none">
         <Sidebar />
       </div>
-      <div className=" flex flex-col  bg-mainDash">
+      <div className="flex-1 w-32">
         <HeaderDash />
-        <Filters />
-        <div>
-          <DashProducts />
+        <div className="my-8">
+          <SearchBar />
+          <div className="flex mt-10 justify-evenly my-8">
+            <FilterModal />
+            <FilterModal />
+            <FilterModal />
+          </div>
+          <div className="flex justify-center">
+            <DashProducts />
+          </div>
         </div>
       </div>
     </div>
