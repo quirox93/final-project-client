9ac5a7cc8b6b35d889d7259819c9c925868c7b46
Luciano E. Perez <info.luciando.design@gmail.com>
2023-08-16 17:36:19 -0300

    build carrito, y reducer

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,8 +1,14 @@
 // app/providers.tsx
 "use client";
 
+import store from "@/store/store";
 import { NextUIProvider } from "@nextui-org/react";
+import { Provider } from "react-redux";
 
 export function Providers({ children }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <Provider store={store}>{children}</Provider>
+    </NextUIProvider>
+  );
 }
