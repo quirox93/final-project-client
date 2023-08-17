cd4b2726997743331df2878ddaa087117bac8172
warben <quirox01@gmail.com>
2023-08-17 14:01:14 -0300

    test2

--- src/components/AdminProducts/ProductsTable.jsx
+++ src/components/AdminProducts/ProductsTable.jsx
@@ -2,12 +2,12 @@ import AdminProducts from "@/components/AdminProducts/AdminProducts";
 import api from "@/utils/axios";
 
 export default async function ProductsTable() {
-  const { data } = await api.get("product");
-  const users = data.results;
+  //const { data } = await api.get("product");
+  const users = [];
   return (
     <>
       <div className="p-4 z-0 flex flex-col relative justify-between gap-4 bg-content1 overflow-auto rounded-large shadow-small max-w-ld ">
-        
+        <AdminProducts defUsers={users} />
       </div>
     </>
   );
