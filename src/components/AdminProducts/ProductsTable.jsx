30a0f6aadcfe6407b6d076326f48b9a015c6908f
warben <quirox01@gmail.com>
2023-08-18 14:30:36 -0300

    Update (ProductsTable)

--- src/components/AdminProducts/ProductsTable.jsx
+++ src/components/AdminProducts/ProductsTable.jsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react";
 import AdminProducts from "./AdminProducts";
 import api from "@/utils/api";
+import LoadingPage from "@/app/loading";
 
 const loadProducts = async () => await api.products();
 
 export default async function ProductsTable() {
   const products = await loadProducts();
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingPage />}>
       <div className="p-4 z-0 flex flex-col relative justify-between gap-4 bg-content1 overflow-auto rounded-large shadow-small max-w-ld ">
         {<AdminProducts defUsers={products.results} />}
       </div>
