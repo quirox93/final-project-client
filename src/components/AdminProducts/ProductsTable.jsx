9171c90da0aeb1c75499d8ef55211bb2672ace2c
warben <quirox01@gmail.com>
2023-08-18 11:49:42 -0300

    fix tested

--- src/components/AdminProducts/ProductsTable.jsx
+++ src/components/AdminProducts/ProductsTable.jsx
@@ -1,16 +1,20 @@
-import api from "@/utils/api";
+import { headers } from "next/headers";
 import AdminProducts from "./AdminProducts";
 
-async function loadProducts() {
-  const data = await fetch("http://localhost:3000/api/product");
-  return await data.json();
-}
+const loadProducts = async () => {
+  const host = headers().get("host");
+  const res = await fetch(`http://${host}/api/product`);
+  const data = await res.json();
+  return data;
+};
 
 export default async function ProductsTable() {
   const products = await loadProducts();
   return (
-    <div className="p-4 z-0 flex flex-col relative justify-between gap-4 bg-content1 overflow-auto rounded-large shadow-small max-w-ld ">
-      {<AdminProducts defUsers={products.results} />}
-    </div>
+    <>
+      <div className="p-4 z-0 flex flex-col relative justify-between gap-4 bg-content1 overflow-auto rounded-large shadow-small max-w-ld ">
+        {<AdminProducts defUsers={products.results} />}
+      </div>
+    </>
   );
 }
