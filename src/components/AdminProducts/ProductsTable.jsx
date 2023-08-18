037e389df764d2e4f9fdcd9ffacf45214518d54e
warben <quirox01@gmail.com>
2023-08-18 10:34:27 -0300

    fix (dashboard) Error using server component

--- src/components/AdminProducts/ProductsTable.jsx
+++ src/components/AdminProducts/ProductsTable.jsx
@@ -1,24 +1,17 @@
-"use client";
-import LoadingPage from "@/app/loading";
-import AdminProducts from "@/components/AdminProducts/AdminProducts";
-import api from "@/utils/axios";
-import { useEffect, useState } from "react";
+import { headers } from "next/headers";
+const loadProducts = async () => {
+  const host = headers().get("host");
+  const res = await fetch(`http://${host}/api/product`);
+  const data = await res.json();
+  return data;
+};
 
-export default function ProductsTable() {
-  const [users, setUsers] = useState([]);
-  const getData = () => {
-    const fetchData = async () => {
-      const { data } = await api.get("product");
-      setUsers(data.results);
-    };
-    fetchData();
-  };
-  useEffect(getData, []);
-
+export default async function ProductsTable() {
+  const products = await loadProducts();
   return (
     <>
       <div className="p-4 z-0 flex flex-col relative justify-between gap-4 bg-content1 overflow-auto rounded-large shadow-small max-w-ld ">
-        {users.length ? <AdminProducts defUsers={users} /> : <LoadingPage />}
+        {products.results.toString()}
       </div>
     </>
   );
