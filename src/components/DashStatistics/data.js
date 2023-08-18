dc6c055ddd40d200524dd994f1856fac780f8222
camiloduquee <camiloduquee@gmail.com>
2023-08-18 05:04:06 -0400

    Create: Statistics and Update: Dashboard

---
+++ src/components/DashStatistics/data.js
@@ -0,0 +1,78 @@
+export const data = [
+    {
+      id: 1,
+      name: {
+        first: 'camilo',
+        last: 'Gomez',
+      },
+      total: 500.95,
+      status: 'On Hold',
+      method: 'Mercado Pago',
+      date: '5 Minutes ago',
+    },
+    {
+      id: 2,
+      name: {
+        first: 'Matias',
+        last: 'Solis',
+      },
+      total: 2500,
+      status: 'Processing',
+      method: 'PayPal',
+      date: '12 Minutes ago',
+    },
+    {
+      id: 3,
+      name: {
+        first: 'Monica',
+        last: 'Garcia',
+      },
+      total: 1450.15,
+      status: 'Completed',
+      method: 'Mercado Pago',
+      date: '1 Hour ago',
+    },
+    {
+      id: 4,
+      name: {
+        first: 'Manuel',
+        last: 'Torrealba',
+      },
+      total: 150.45,
+      status: 'Processing',
+      method: 'Bitcoin',
+      date: '1 Hour ago',
+    },
+    {
+      id: 5,
+      name: {
+        first: 'Luciano',
+        last: 'Perez',
+      },
+      total: 900.45,
+      status: 'On Hold',
+      method: 'Bitcoin',
+      date: '3 Hour ago',
+    },
+    {
+      id: 6,
+      name: {
+        first: 'Brian',
+        last: 'Quiroz',
+      },
+      total: 1295.75,
+      status: 'Completed',
+      method: 'Mercado Pago',
+      date: '4 Hour ago',
+    },
+    {
+      id: 7,
+      name: {
+        first: 'Erick',
+        last: 'Segura',
+      },
+      total: 500.95,
+      status: 'Completed',
+      method: 'Paypal',
+      date: '4 Hour ago',
+    }]
