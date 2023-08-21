cda37c96003c24f1fdd3cbda9c80538775cfec87
Monicamegarcia <monicamegarcia@gmail.com>
2023-08-21 12:30:15 -0300

    clerk api user methods

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -1,11 +1,20 @@
 const { NextResponse } = require("next/server");
 import { connectDB } from "@/utils/mongoose";
+import { clerkClient} from "@clerk/nextjs";
 
 export async function GET(_, { params }) {
   try {
     connectDB();
     const { id } = params;
-    return NextResponse.json({ message: "Obtener datos del id: " + id });
+
+    // Obtener el usuario
+    const user = await clerkClient.users.getUser(id);
+
+    if (!user) {
+      return NextResponse.json({ error: "El usuario no existe" });
+    }
+
+    return NextResponse.json({ message: "Usuario encontrado: " + user });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
@@ -15,7 +24,15 @@ export async function DELETE(_, { params }) {
   try {
     connectDB();
     const { id } = params;
-    return NextResponse.json({ error: "Borrar usuario con id: " + id });
+
+    // Eliminar el usuario
+    const user = await clerkClient.users.deleteUser(id);
+
+    if (!user) {
+      return NextResponse.json({ error: "El usuario no existe" });
+    }
+
+    return NextResponse.json({ message: "Usuario eliminado" });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,10 +1,12 @@
 const { NextResponse } = require("next/server");
 import { connectDB } from "@/utils/mongoose";
+import { clerkClient} from "@clerk/nextjs";
 
 export async function GET() {
   try {
     connectDB();
-    return NextResponse.json({ message: "Devolver todos los usuarios." });
+    const users = await clerkClient.users.getUserList();
+    return NextResponse.json(users);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
