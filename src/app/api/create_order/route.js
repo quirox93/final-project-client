3261c084a75b82f5472d86428f0efe06250a7dc7
ManuelSeoTorr <manst94@gmail.com>
2023-08-21 00:21:19 -0300

    update:PasarelaDePagosMP

--- src/app/api/create_order/route.js
+++ src/app/api/create_order/route.js
@@ -4,37 +4,42 @@ import mercadopago from "mercadopago";
 const items = [
   {
     title: "card1",
-    unite_price: "25",
+    unit_price: 25,
     quantity: 1,
-    currency_id: "USD",
+    currency_id: "ARS",
   },
   {
     title: "card2",
-    unite_price: "15",
+    unit_price: 15,
     quantity: 1,
-    currency_id: "USD",
+    currency_id: "ARS",
   },
   {
     title: "card3",
-    unite_price: "35",
+    unit_price: 35,
     quantity: 1,
-    currency_id: "USD",
+    currency_id: "ARS",
   },
 ];
-export async function GET(req) {
+export async function POST() {
   mercadopago.configure({
     access_token:
       "TEST-5829389646462984-081811-95d54801b0a8fa9664b3e903edd0bbec-1453720691",
   });
-
- const result = await mercadopago.preferences.create(items);
-
- console.log(result)
-  // try {
-
-  //     console.log("queres crear una orden")
-  //   return NextResponse.json({ message: "create_order"});
-  // } catch (error) {
-  //   return NextResponse.json({ error: error.message }, { status: 500 });
-  // }
+try {
+  const result = await mercadopago.preferences.create({
+    items,
+    back_urls:{
+      success:"",
+      failure:"",
+      pending:"",
+    },
+    notification_url:"http://localhost:3000/api/webhook"
+  });
+  console.log(result)
+  return NextResponse.json(result);
+  
+} catch (error) {
+  return NextResponse.json({ error: error.message }, { status: 500 });
+}
 }
---
+++ src/app/api/webhook/route.js
@@ -0,0 +1,6 @@
+const { NextRequest, NextResponse } = require("next/server");
+import mercadopago from "mercadopago";
+
+export async function POST(req) {
+console.log (req.query);
+}
