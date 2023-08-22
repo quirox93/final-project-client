0e2a4e084d20bce3e1efd66cec42c36222e39901
warben <quirox01@gmail.com>
2023-08-21 22:06:53 -0300

    f

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -1,8 +1,7 @@
 const { NextResponse } = require("next/server");
-import { MP_TOKEN } from "@/utils/config";
+import { MP_TOKEN, VERCEL_URL } from "@/utils/config";
 import getURL from "@/utils/getUrl";
 import mercadopago from "mercadopago";
-const host = getURL("api");
 
 export async function POST(req) {
   const { items, userId } = await req.json();
@@ -10,12 +9,14 @@ export async function POST(req) {
     access_token: MP_TOKEN,
   });
   try {
+    const host = getURL("api");
+    console.log({ VERCEL_URL, host });
     const result = await mercadopago.preferences.create({
       items,
       back_urls: {
-        success: `${host}/cart`,
-        failure: `${host}/cart`,
-        pending: `${host}/cart`,
+        success: `${VERCEL_URL}/cart`,
+        failure: `${VERCEL_URL}/cart`,
+        pending: `${VERCEL_URL}/cart`,
       },
       //notification_url:"http://localhost:3000/api/webhook"
     });
