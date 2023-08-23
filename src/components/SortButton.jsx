5c7fae1327ab99711ad3f483f7ed41dc80c92c68
warben <quirox01@gmail.com>
2023-08-23 13:44:39 -0300

    update(sortButton)

--- src/components/SortButton.jsx
+++ src/components/SortButton.jsx
@@ -10,90 +10,36 @@ import {
 } from "@nextui-org/react";
 
 export default function SortButton({ onSortChange }) {
-  const [selectedKeys, setSelectedKeys] = useState(new Set(["Sort by:"]));
-  const [disabledKeys, setDisabledKeys] = useState([]);
+  const [selectedKeys, setSelectedKeys] = useState(new Set(["Price", "High"]));
 
-  const price = "Price";
-  const name = "Name";
-  const high = "High";
-  const low = "Low";
-  const z_a = "Z-A";
-  const a_z = "A-Z";
+  const SORTS = {
+    Price: {
+      Low: "priceAsc",
+      High: "priceDesc",
+    },
+    Name: {
+      "A-Z": "nameAsc",
+      "Z-A": "nameDesc",
+    },
+  };
 
-  const isPriceSelected = selectedKeys.has(price);
-  const isNameSelected = selectedKeys.has(name);
-  const isHighSelected = selectedKeys.has(high);
-  const isLowSelected = selectedKeys.has(low);
-  const isAzSelected = selectedKeys.has(a_z);
-  const isZaSelected = selectedKeys.has(z_a);
-
-  useMemo(() => {
-    if (isPriceSelected) {
-      setDisabledKeys([name]);
-    } else if (isNameSelected) {
-      setDisabledKeys([price]);
-    } else {
-      setDisabledKeys([]);
-    }
-  }, [isPriceSelected, isNameSelected]);
-
-  useMemo(() => {
-    if (isHighSelected) {
-      setDisabledKeys((prevDisabledKeys) => [
-        ...prevDisabledKeys,
-        low,
-        name,
-        price,
-      ]);
-    } else if (isLowSelected) {
-      setDisabledKeys((prevDisabledKeys) => [
-        ...prevDisabledKeys,
-        high,
-        name,
-        price,
-      ]);
-    } else {
-      setDisabledKeys((prevDisabledKeys) =>
-        prevDisabledKeys.filter(
-          (key) => key !== high && key !== low && key !== price
-        )
-      );
-    }
-  }, [isHighSelected, isLowSelected]);
-
-  useMemo(() => {
-    if (isAzSelected) {
-      setDisabledKeys((prevDisabledKeys) => [
-        ...prevDisabledKeys,
-        z_a,
-        price,
-        name,
-      ]);
-    } else if (isZaSelected) {
-      setDisabledKeys((prevDisabledKeys) => [
-        ...prevDisabledKeys,
-        a_z,
-        price,
-        name,
-      ]);
-    } else {
-      setDisabledKeys((prevDisabledKeys) =>
-        prevDisabledKeys.filter(
-          (key) => key !== a_z && key !== z_a && key !== name
-        )
-      );
-    }
-  }, [isAzSelected, isZaSelected]);
+  const isPriceSelected = selectedKeys.has("Price");
+  const isNameSelected = selectedKeys.has("Name");
 
   const selectedValue = useMemo(
     () => Array.from(selectedKeys).join(" ").replaceAll("_", " "),
     [selectedKeys]
   );
 
-  const handleSortChange = (sortType) => {
-    onSortChange(sortType);
+  const handleChange = ({ currentKey }) => {
+    let newSet;
+    if (Object.keys(SORTS).includes(currentKey))
+      newSet = [currentKey, Object.keys(SORTS[currentKey])[0]];
+    else newSet = [[...selectedKeys][0], currentKey];
+    setSelectedKeys(new Set(newSet));
+    onSortChange(SORTS[newSet[0]][newSet[1]]);
+    console.log(SORTS[newSet[0]][newSet[1]]);
   };
-
   return (
     <Dropdown>
       <DropdownTrigger>
@@ -110,9 +56,8 @@ export default function SortButton({ onSortChange }) {
         aria-label="Dropdown menu with description"
         disallowEmptySelection
         selectedKeys={selectedKeys}
-        disabledKeys={disabledKeys}
         selectionMode="multiple"
-        onSelectionChange={setSelectedKeys}
+        onSelectionChange={handleChange}
         closeOnSelect={false}
       >
         <DropdownSection title="Sort by:" showDivider>
@@ -121,36 +66,20 @@ export default function SortButton({ onSortChange }) {
         </DropdownSection>
         {isPriceSelected && (
           <DropdownSection title="Price Order:">
-            <DropdownItem
-              key="High"
-              color="success"
-              onPress={() => handleSortChange("priceDesc")}
-            >
+            <DropdownItem key="High" color="success">
               High
             </DropdownItem>
-            <DropdownItem
-              key="Low"
-              color="danger"
-              onPress={() => handleSortChange("priceAsc")}
-            >
+            <DropdownItem key="Low" color="danger">
               Low
             </DropdownItem>
           </DropdownSection>
         )}
         {isNameSelected && (
           <DropdownSection title="Name Order:">
-            <DropdownItem
-              key="A-Z"
-              color="success"
-              onPress={() => handleSortChange("nameAsc")}
-            >
+            <DropdownItem key="A-Z" color="success">
               A-Z
             </DropdownItem>
-            <DropdownItem
-              key="Z-A"
-              color="danger"
-              onPress={() => handleSortChange("nameDesc")}
-            >
+            <DropdownItem key="Z-A" color="danger">
               Z-A
             </DropdownItem>
           </DropdownSection>
