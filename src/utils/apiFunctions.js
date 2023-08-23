12ad166cecbabe9f6424a16e76b12c80767549a5
warben <quirox01@gmail.com>
2023-08-23 14:16:33 -0300

    refactor(sort types)

--- src/utils/apiFunctions.js
+++ src/utils/apiFunctions.js
@@ -35,43 +35,6 @@ const filterByName = (items, value) => {
   return items.filter((producto) => producto.name.toLowerCase().includes(searchTerm));
 };
 
-const sortByName = (items, order = "nameAsc") => {
-  const sortedItems = [...items]; // Crear una copia del arreglo para no modificar el original
-
-  sortedItems.sort((a, b) => {
-    const nameA = a.name.toLowerCase();
-    const nameB = b.name.toLowerCase();
-
-    if (order === "nameAsc") {
-      if (nameA < nameB) return -1;
-      if (nameA > nameB) return 1;
-    } else if (order === "nameDesc") {
-      if (nameA > nameB) return -1;
-      if (nameA < nameB) return 1;
-    }
-
-    return 0;
-  });
-
-  return sortedItems;
-};
-
-const sortByPrice = (items, order = "priceAsc") => {
-  const sortedItems = [...items]; // Crear una copia del arreglo para no modificar el original
-
-  sortedItems.sort((a, b) => {
-    if (order === "priceAsc") {
-      return a.price - b.price;
-    } else if (order === "priceDesc") {
-      return b.price - a.price;
-    }
-
-    return 0;
-  });
-
-  return sortedItems;
-};
-
 const SortFunc = {
   dateAsc: (a, b) => a.createdAt.localeCompare(b.createdAt),
   dateDesc: (a, b) => b.createdAt.localeCompare(a.createdAt),
