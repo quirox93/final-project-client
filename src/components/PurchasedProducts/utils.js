0092c01bfe1c09e79ebbf295a9b974e1846f911b
Luciano E. Perez <info.luciando.design@gmail.com>
2023-08-29 15:09:31 -0300

    purchased section

---
+++ src/components/PurchasedProducts/utils.js
@@ -0,0 +1,4 @@
+export const formatDate = (date) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
