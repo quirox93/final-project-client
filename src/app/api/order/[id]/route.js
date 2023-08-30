095bb212180248f8df792db05c2072150e2df39f
freeluckperson <gestionecesaria@gmail.com>
2023-08-30 10:46:22 -0400

    el get by clerkId

--- src/app/api/order/[id]/route.js
+++ src/app/api/order/[id]/route.js
@@ -1,14 +1,14 @@
 const { NextResponse } = require("next/server");
 import { connectDB } from "@/utils/mongoose";
+import orders from "@/models/Order";
 
-export async function GET(_, { params }) {
+export async function GET(_, { params: { id } }) {
   try {
     connectDB();
-    const { id } = params;
-
     // Obtener la orden
-
-    return NextResponse.json({ message: "Buscar orden por id" });
+    const order = await orders.find({ "payer.clerkId": id });
+    if (!order) return NextResponse.json({ message: "Order not found " }, { status: 404 });
+    return NextResponse.json(order);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
