b10fa6b443d1636772316448cb83b7114409aa6b
warben <quirox01@gmail.com>
2023-09-02 16:32:18 -0300

    update(middleware)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 import NavBar from "../components/NavBar/NavBar";
-import { ClerkProvider, auth } from "@clerk/nextjs";
+import { ClerkProvider, auth, clerkClient } from "@clerk/nextjs";
 import { CLERK_ORG_ID } from "@/utils/config";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,10 +12,13 @@ export const metadata = {
   description: "Trading Card Game seller",
 };
 
-export default function RootLayout({ children }) {
-  let isAdmin = false;
-  const { sessionClaims } = auth();
-  if (sessionClaims) isAdmin = Object.keys(sessionClaims?._orgs).includes(CLERK_ORG_ID);
+export default async function RootLayout({ children }) {
+  let isAdmin = true;
+  const data = auth();
+  if (data.userId) {
+    const user = await clerkClient.users.getUser(data.userId);
+    isAdmin = user.publicMetadata.isAdmin;
+  }
   return (
     <html lang="en" className="light text-foreground bg-background">
       <body className={inter.className}>
---
+++ src/components/UsersTable/UsersInfo.jsx
@@ -0,0 +1,300 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Input,
+  Button,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Chip,
+  User,
+  Pagination,
+  ButtonGroup,
+  Spacer,
+} from "@nextui-org/react";
+import { SearchIcon } from "../AdminProducts/SearchIcon";
+import { ChevronDownIcon } from "../AdminProducts/ChevronDownIcon";
+import { capitalize } from "../AdminProducts/utils";
+import ProdButtonGroup from "../AdminProducts/ProdButtonGroup";
+import MultiProdButtonGroup from "../AdminProducts/MultiProdButtonGroup";
+import { formatDate } from "../PurchasedProducts/utils";
+
+const statusColorMap = {
+  true: "success",
+  false: "warning",
+};
+
+const sort = {
+  nameascending: (a, b) =>
+    (a.firstName || "ZZZZ").localeCompare(b.firstName || "ZZZZ", "en", {
+      sensitivity: "base",
+    }),
+  namedescending: (a, b) =>
+    (b.firstName || "").localeCompare(a.firstName || "", "en", {
+      sensitivity: "base",
+    }),
+  createdAtascending: (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+  createdAtdescending: (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+};
+
+export default function UsersInfo({ defItems, columns, statusOptions, INITIAL_VISIBLE_COLUMNS }) {
+  const [allItems, setAllItems] = React.useState(defItems);
+  const [filterValue, setFilterValue] = React.useState("");
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set([]));
+  const [visibleColumns, setVisibleColumns] = React.useState(new Set(INITIAL_VISIBLE_COLUMNS));
+  const [statusFilter, setStatusFilter] = React.useState("all");
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [sortDescriptor, setSortDescriptor] = React.useState({
+    column: "name",
+    direction: "ascending",
+  });
+
+  const [page, setPage] = React.useState(1);
+
+  const hasSearchFilter = Boolean(filterValue);
+
+  const headerColumns = React.useMemo(() => {
+    if (visibleColumns === "all") return columns;
+
+    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
+  }, [visibleColumns]);
+
+  const filteredItems = React.useMemo(() => {
+    const sortF = sort[sortDescriptor.column + sortDescriptor.direction];
+    let filteredUsers = [...allItems].sort(sortF);
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter((item) => {
+        const name = `${item.firstName || ""} ${item.lastName || ""}`;
+        console.log(name);
+        return name.toLowerCase().includes(filterValue.toLowerCase());
+      });
+    }
+    if (statusFilter !== "all" && Array.from(statusFilter).length !== statusOptions.length) {
+      filteredUsers = filteredUsers.filter((item) =>
+        Array.from(statusFilter).includes(item[statusOptions[0].prop].toString())
+      );
+    }
+
+    return filteredUsers;
+  }, [allItems, filterValue, statusFilter, sortDescriptor]);
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return filteredItems.slice(start, end);
+  }, [page, filteredItems, rowsPerPage]);
+
+  const renderCell = React.useCallback((item, columnKey) => {
+    const cellValue = item[columnKey];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: item.profileImageUrl }}
+            description={item.emailAddresses[0].emailAddress}
+            name={`${item.firstName || ""} ${item.lastName || ""}`}
+          >
+            {item.emailAddresses[0].emailAddress}
+          </User>
+        );
+      case "createdAt":
+        return formatDate(cellValue);
+
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  const onNextPage = React.useCallback(() => {
+    if (page < pages) {
+      setPage(page + 1);
+    }
+  }, [page, pages]);
+
+  const onPreviousPage = React.useCallback(() => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }, [page]);
+
+  const onRowsPerPageChange = React.useCallback((e) => {
+    setRowsPerPage(Number(e.target.value));
+    setPage(1);
+  }, []);
+
+  const onSearchChange = React.useCallback((value) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const onClear = React.useCallback(() => {
+    setFilterValue("");
+    setPage(1);
+  }, []);
+
+  const topContent = React.useMemo(() => {
+    let selected = Array.from(selectedKeys);
+
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between gap-3 items-end">
+          <Input
+            isClearable
+            className="w-full sm:max-w-[44%]"
+            placeholder="Search by name..."
+            startContent={<SearchIcon />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+          <div className="flex gap-3">
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+                  Status
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={statusFilter}
+                selectionMode="multiple"
+                onSelectionChange={setStatusFilter}
+              >
+                {statusOptions.map((status) => (
+                  <DropdownItem key={status.uid} className="capitalize">
+                    {capitalize(status.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+                  Columns
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={visibleColumns}
+                selectionMode="multiple"
+                onSelectionChange={setVisibleColumns}
+              >
+                {columns.map((column) => (
+                  <DropdownItem key={column.uid} className="capitalize">
+                    {capitalize(column.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-default-400 text-small">Total {allItems.length} products</span>
+          <label className="flex items-center text-default-400 text-small">
+            Rows per page:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    );
+  }, [
+    filterValue,
+    onSearchChange,
+    statusFilter,
+    visibleColumns,
+    allItems,
+    onRowsPerPageChange,
+    onClear,
+    selectedKeys,
+  ]);
+
+  const bottomContent = React.useMemo(() => {
+    return (
+      <div className="py-2 px-2 flex justify-between items-center">
+        <span className="w-[30%] text-small text-default-400">
+          {selectedKeys === "all"
+            ? "All items selected"
+            : `${selectedKeys.size} of ${filteredItems.length} selected`}
+        </span>
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="primary"
+          page={page}
+          total={pages}
+          onChange={setPage}
+        />
+        <div className="hidden sm:flex w-[30%] justify-end gap-2">
+          <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onPreviousPage}>
+            Previous
+          </Button>
+          <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onNextPage}>
+            Next
+          </Button>
+        </div>
+      </div>
+    );
+  }, [selectedKeys, items.length, page, pages, hasSearchFilter]);
+
+  return (
+    <Table
+      aria-label="Items table"
+      isHeaderSticky
+      bottomContent={bottomContent}
+      bottomContentPlacement="outside"
+      classNames={{
+        wrapper: "max-h-[400px]",
+      }}
+      selectedKeys={selectedKeys}
+      selectionMode="single"
+      sortDescriptor={sortDescriptor}
+      topContent={topContent}
+      topContentPlacement="outside"
+      onSelectionChange={setSelectedKeys}
+      onSortChange={setSortDescriptor}
+    >
+      <TableHeader columns={headerColumns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={"start"} allowsSorting={column.sortable}>
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody emptyContent={"No items found"} items={items}>
+        {(item) => (
+          <TableRow key={item._id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
