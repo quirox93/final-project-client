01290998e99e12c53d96513a0e1e720c39c1d584
Luciano E. Perez <info.luciando.design@gmail.com>
2023-09-07 13:31:44 -0300

    estilos home y empty purchased

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,7 +10,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateCart } from "@/store/slice";
 import { motion } from "framer-motion";
 
-
 export default function Product({
   id,
   name,
@@ -19,17 +18,15 @@ export default function Product({
   price,
   stock,
   delay,
-  userId
+  userId,
 }) {
   const router = useRouter();
   const dispatch = useDispatch();
-  const cartItems = useSelector(
-    (state) => state.shopCart.cartItems
-  );
+  const cartItems = useSelector((state) => state.shopCart.cartItems);
   const [quantity, setQuantity] = useState(1);
   const [showModal, setShowModal] = useState(false);
   const [popoverOpen, setPopoverOpen] = useState(false);
-  
+
   useEffect(() => {
     let timer;
     if (popoverOpen) {
@@ -44,21 +41,29 @@ export default function Product({
   }, [popoverOpen]);
 
   const handleAddToCartWrapper = () => {
-    handleAddToCart({
-      id,
-      name,
-      image,
-      description,
-      stock,
-      price,
-      quantity,
-      userId
-    }, cartItems, dispatch, setShowModal, setPopoverOpen, updateCart, setQuantity);
+    handleAddToCart(
+      {
+        id,
+        name,
+        image,
+        description,
+        stock,
+        price,
+        quantity,
+        userId,
+      },
+      cartItems,
+      dispatch,
+      setShowModal,
+      setPopoverOpen,
+      updateCart,
+      setQuantity
+    );
   };
-  
+
   return (
     <motion.div
-      className="bg-white m-3 lg:w-3/12 md:w-1/3 flex items-center  p-2 rounded-2xl shadow-2xl sm:m-10"
+      className="bg-white m-3 lg:w-3/12 md:w-1/3 flex-col items-center  p-2 rounded-2xl shadow-2xl sm:m-10"
       initial={{ opacity: 0, x: -20 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.3, delay: delay }}
@@ -68,75 +73,94 @@ export default function Product({
         onClose={() => setShowModal(false)}
         name={name}
       />
-      <div className="flex-1">
-        <img
-          className=" cursor-pointer min-w-[100px]"
-          src={image}
-          alt={name}
-          width={200}
-          height={200}
-          onClick={() => router.push(`/product/${id}`)}
-        />
-      </div>
-      <div className="text-black ml-5 flex-1 space-y-3">
+      <div className="m-auto p-2">
         <h2
-          className="text-lg font-bold cursor-pointer"
+          className="text-lg font-bold cursor-pointer text-center"
           onClick={() => router.push(`/product/${id}`)}
         >
           {name}{" "}
         </h2>
-        <p>{description.length > 35 ? description.substring(0,35) + "..." : description}</p>
-        <Divider />
-        <p>
-          Price: <span className="font-bold">${price}</span>
-        </p>
-        {stock === 0 ? (
-          <Chip className="capitalize" color="danger" size="sm" variant="flat">
-            Out of stock
-          </Chip>
-        ) : (
-          <Chip className="capitalize" color="success" size="sm" variant="flat">
-            Available
-          </Chip>
-        )}
-        <p>
-          Stock:{" "}
-          {stock === 0 ? (
-            <span className="text-red">0</span>
-          ) : (
-            <span className="text-green">{stock}</span>
-          )}
-        </p>
-        <Divider />
-        <div className="flex-column justify-center items-center">
-          <Input
-            type="number"
-            label="Quantity"
-            onChange={(e) => {
-              let inputValue = parseInt(e.target.value);
-              if (isNaN(inputValue) || inputValue < 1) {
-                inputValue = 1;
-              } else if (inputValue > stock) {
-                inputValue = stock;
-              }
-              setQuantity(inputValue);
-            }}
-            value={quantity}
-            color="primary"
-            placeholder="0"
-            labelPlacement="inside"
-            className="mb-2"
-            startContent={
-              <div className="pointer-events-none flex items-center"></div>
-            }
-          />
-          <ProductPopOver
-            popoverOpen={popoverOpen}
-            handleAddToCart={handleAddToCartWrapper}
-            quantity={quantity}
-            stock={stock}
+      </div>
+      <Divider />
+      <div className="flex items-center min-h-[220px]">
+        <div className="flex-1 p-1">
+          <img
+            className=" cursor-pointer min-w-[100px] "
+            src={image}
+            alt={name}
+            width={200}
+            height={200}
+            onClick={() => router.push(`/product/${id}`)}
           />
         </div>
+        <div className="text-black  flex-1 p-1 space-y-3">
+          <p>
+            {description.length > 35
+              ? description.substring(0, 35) + "..."
+              : description}
+          </p>
+          <Divider />
+          <p>
+            Price: <span className="font-bold">${price}</span>
+          </p>
+          {stock === 0 ? (
+            <Chip
+              className="capitalize"
+              color="danger"
+              size="sm"
+              variant="flat"
+            >
+              Out of stock
+            </Chip>
+          ) : (
+            <Chip
+              className="capitalize"
+              color="success"
+              size="sm"
+              variant="flat"
+            >
+              Available
+            </Chip>
+          )}
+          <p>
+            Stock:{" "}
+            {stock === 0 ? (
+              <span className="text-red">0</span>
+            ) : (
+              <span className="text-green">{stock}</span>
+            )}
+          </p>
+          <Divider />
+        </div>
+      </div>
+      <div className="flex-column justify-center items-center p-2">
+        <Input
+          type="number"
+          label="Quantity"
+          onChange={(e) => {
+            let inputValue = parseInt(e.target.value);
+            if (isNaN(inputValue) || inputValue < 1) {
+              inputValue = 1;
+            } else if (inputValue > stock) {
+              inputValue = stock;
+            }
+            setQuantity(inputValue);
+          }}
+          value={quantity}
+          color="primary"
+          placeholder="0"
+          labelPlacement="inside"
+          className="mb-2"
+          startContent={
+            <div className="pointer-events-none flex items-center"></div>
+          }
+        />
+        <ProductPopOver
+          popoverOpen={popoverOpen}
+          handleAddToCart={handleAddToCartWrapper}
+          quantity={quantity}
+          stock={stock}
+        />
       </div>
     </motion.div>
   );
